331162c89f24b5c67922341cfb85fdef024e192f
saielhaq <119542836+saielhaq@users.noreply.github.com>
2023-05-16 23:21:12 +0100

    First Commit
    16-05-2023

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return ( 
+    <Image
+      onClick={() => router.push('/')}
+      className="hidden md:block cursor-pointer" 
+      src="/images/logo.png" 
+      height="60" 
+      width="60" 
+      alt="Logo" 
+    />
+   );
+}
+ 
+export default Logo;
